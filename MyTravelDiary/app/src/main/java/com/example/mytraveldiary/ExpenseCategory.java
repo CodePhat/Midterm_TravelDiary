86f12f0331327af04abbd2bad207c6398c1f4852
New User Name <lephatvip6@gmail.com>
2025-10-17 20:59:17 +0700

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	MyTravelDiary/.idea/deploymentTargetSelector.xml
    #	MyTravelDiary/.idea/gradle.xml
    #	MyTravelDiary/.idea/misc.xml
    #	MyTravelDiary/app/src/main/AndroidManifest.xml
    #	MyTravelDiary/app/src/main/java/com/example/mytraveldiary/AppData.java
    #	MyTravelDiary/app/src/main/java/com/example/mytraveldiary/ExpenseCategory.java
    #	MyTravelDiary/app/src/main/java/com/example/mytraveldiary/MainActivity.java
    #	MyTravelDiary/app/src/main/java/com/example/mytraveldiary/TripDetailFragment.java
    #	MyTravelDiary/app/src/main/res/layout/activity_main.xml
    #	MyTravelDiary/app/src/main/res/layout/dialog_add_expense.xml
    #	MyTravelDiary/app/src/main/res/layout/fragment_trip_detail.xml
    #	MyTravelDiary/app/src/main/res/layout/fragment_trips.xml

--- MyTravelDiary/app/src/main/java/com/example/mytraveldiary/ExpenseCategory.java
+++ MyTravelDiary/app/src/main/java/com/example/mytraveldiary/ExpenseCategory.java
@@ -1,6 +1,6 @@
 package com.example.mytraveldiary;
 
 public enum ExpenseCategory {
-    Food, Transport, Accommodation, Entertainment, Shopping, OTHER, ACTIVITY
+    Food, Transport, Accommodation, Entertainment, Shopping, Other, Activity
 }
 
